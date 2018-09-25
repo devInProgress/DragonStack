855d26dd6169973f34095d00cf4684700c11c5a6
Naved <naved1234@gmail.com>
2018-09-25 12:51:01 +0530

    code addition

--- Backend/dragon.js
+++ Backend/dragon.js
@@ -1,7 +1,14 @@
+const DEFAULT_PROPERTIES = {
+  get birthdate() {
+    return new Date();
+  },
+  nickname: 'unnamed'
+};
+
 class Dragon {
-  constructor(birthdate, nickname) {
-    this.birthdate = birthdate;
-    this.nickname = nickname;
+  constructor({ birthdate, nickname } = {}) {
+    this.birthdate = birthdate || DEFAULT_PROPERTIES.birthdate;
+    this.nickname = nickname || DEFAULT_PROPERTIES.nickname;
   }
 }
 
